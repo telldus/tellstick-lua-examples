0a2d4f24ad1a787492203c6e1e618aa4a12eafe7
Fredrik Gullberg <fredrik.gullberg@telldus.se>
2018-04-19 11:06:59 +0200

    Add Meter Reset script

---
+++ zwave/MeterReset.lua
@@ -0,0 +1,33 @@
+-- Script to send a Meter Reset command to a Z-Wave device 
+-- This can be handy to reset the accomulated power readings in a plug
+-- The reset command will be sent to the device below when the trigger is controlled ON
+
+local deviceName = "Plug"    -- name of your device
+local trigger = "MeterReset" -- Name of the trigger
+
+------ Do not change below ------
+COMMAND_CLASS_METER = 0x32
+METER_RESET = 0x05
+
+local deviceManager = require "telldus.DeviceManager"
+
+function onDeviceStateChanged(device, state, stateValue)
+	local trig = deviceManager:findByName(trigger)
+	local dev = deviceManager:findByName(deviceName)
+	if device:id() ~= trig:id() then
+		return
+	end
+	if trig == nil then
+		print("Could not find the device %s", trigger)
+		return
+	end
+	if dev == nil then
+		print("Could not find the device %s", trigger)
+		return
+	end
+	if trig:state() == 1 then
+		print("Sending METER RESET to %s", deviceName)
+		dev:zwaveNode():sendMsg(COMMAND_CLASS_METER, METER_RESET)
+	end
+end
+
