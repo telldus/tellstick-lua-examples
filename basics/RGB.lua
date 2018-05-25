7b422d072c7cdc3b58cec5a52e07a118d084e90f
Fredrik Gullberg <fredrik.gullberg@telldus.se>
2018-05-25 13:46:18 +0200

    Add Define and Do not change below

--- basics/RGB.lua
+++ basics/RGB.lua
@@ -1,6 +1,11 @@
-local deviceManager = require "telldus.DeviceManager"
+-- Script to set RGB color to a device
+
+-- Define your device and settings here:
 local trigger = deviceManager:findByName('red') -- Name of the trigger
 local rgb = deviceManager:findByName('rgb') -- Name of the rgb-device
+
+------ Do not change below ------
+local deviceManager = require "telldus.DeviceManager"
 
 function onDeviceStateChanged(device, state, stateValue)
 	if trigger == nil then
