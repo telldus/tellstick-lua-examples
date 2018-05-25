c13f4a250a842fe4f216fa9f8d517f6a10bc940a
Fredrik Gullberg <fredrik.gullberg@telldus.se>
2018-05-25 13:39:03 +0200

    Add Define and Do not change below

--- basics/SimpleTimer.lua
+++ basics/SimpleTimer.lua
@@ -1,9 +1,12 @@
 -- Turns off a device xx minutes after it was turned on
 
-local deviceManager = require "telldus.DeviceManager"
+-- Define your device and settings here:
 local trigger = "Office" -- Name of the device
 local delay_minutes = 0 -- Delay in minutes
 local delay_seconds = 5 -- Delay in seconds
+
+------ Do not change below ------
+local deviceManager = require "telldus.DeviceManager"
 local running = false
 
 function onDeviceStateChanged(device, state, stateValue)
