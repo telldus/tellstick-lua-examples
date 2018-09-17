81d80f7a6099f681b47b8d6a1b90f32b1d86302c
Fredrik Gullberg <fredrik.gullberg@telldus.se>
2018-09-17 13:20:01 +0200

    Add more to description, add some debug and change from rgbw to rgb

--- basics/RGB.lua
+++ basics/RGB.lua
@@ -1,21 +1,27 @@
 -- Script to set RGB color to a device
+-- The device "RGB" will be red when dummy device red is changing status
 
 -- Define your device and settings here:
-local trigger = deviceManager:findByName('red') -- Name of the trigger
-local rgb = deviceManager:findByName('rgb') -- Name of the rgb-device
+local deviceName = "RGB"    -- Name of your RGB device
+local trigger = "red"       -- Name of your trigger
 
 ------ Do not change below ------
 local deviceManager = require "telldus.DeviceManager"
 
 function onDeviceStateChanged(device, state, stateValue)
-	if trigger == nil then
+	local rgb = deviceManager:findByName(deviceName)
+	local trig = deviceManager:findByName(trigger)
+	if trig == nil then
+		print("Could not find the device %s", trigger)
 		return
 	end
 	if rgb == nil then
+		print("Could not find the device %s", deviceName)
 		return
 	end
-	if device:id() ~= trigger:id() then
+	if device:id() ~= trig:id() then
 		return
 	end
-	rgb:command("rgbw", 0xFF000000, "RGB")
+	print("Set %s to red", rgb:name())
+	rgb:command("rgb", 0xFF0000, "RGB.lua")
 end
